b2b9505d7b3de3ef6311ce8f6244da11f6251bcb
AndreiSmazin <smazinandrey163@gmail.com>
2023-12-22 23:02:23 +0300

    Feat: добавил ErrorHandlerControllerAdvice

--- ewm-stats/ewm-stats-service/src/main/java/ru/practicum/ewm/exception/ErrorHandlerControllerAdvice.java
+++ ewm-stats/ewm-stats-service/src/main/java/ru/practicum/ewm/exception/ErrorHandlerControllerAdvice.java
@@ -1,4 +1,21 @@
 package ru.practicum.ewm.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
 public class ErrorHandlerControllerAdvice {
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ExceptionViolation onThrowable(Throwable e) {
+        log.error("Unpredictable error: {}", e.getMessage());
+
+        return new ExceptionViolation(e.getMessage());
+    }
 }
